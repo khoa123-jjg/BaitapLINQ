2ef4ce5d8c79e5128502a1dd6e0ecee8262647af
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-19 22:02:26 +0700

    bài 11

---
+++ bài 11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace bài_11
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string name;
+        public Double score;
+        public Student(int Id, string name, Double score)
+        {
+            this.Id = Id;
+            this.name = name;
+            this.score = score;
+        }
+        public void In()
+        {
+            Console.WriteLine("Id: " + Id + " Name: " + name + " Score: " + score);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            List<Student> students = new List<Student>()
+            {   new Student (1,"An",8),
+                new Student (2,"Binh",6 ),
+                new Student (3,"Chi",9 ),
+                new Student (4,"Dung",7 )
+            };
+            var student = students.OrderByDescending(s => s.score).Take(1);
+            Console.WriteLine("Sinh viên có điểm cao nhất:");
+            foreach (var s in student)
+            {
+                s.In();
+            }
+            Console.ReadKey();
+        }
+    }
+}
