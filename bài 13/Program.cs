c2ed1d2281a8655cd14336dbb6f079f85d7e257f
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-19 22:08:59 +0700

    bài 13

---
+++ bài 13/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace bài_13
+{
+    using System.Text;
+
+    namespace bài_12
+    {
+        class Student
+        {
+            public int Id { get; set; }
+            public string name;
+            public Double score;
+            public Student(int Id, string name, Double score)
+            {
+                this.Id = Id;
+                this.name = name;
+                this.score = score;
+            }
+            public void In()
+            {
+                Console.WriteLine("Id: " + Id + " Name: " + name + " Score: " + score);
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.InputEncoding = Encoding.UTF8;
+                Console.WriteLine("Phạm Nhật Khoa");
+                Console.WriteLine("2415053122221");
+                List<Student> students = new List<Student>()
+            {   new Student (1,"An",8),
+                new Student (2,"Binh",6 ),
+                new Student (3,"Chi",9 ),
+                new Student (4,"Dung",7 )
+            };
+                var student = students.Where(s => s.score == 7).FirstOrDefault();
+                Console.WriteLine("Sinh viên đầu tiên dạt điểm 7: ");
+                student.In();
+                Console.ReadKey();
+            }
+        }
+    }
+}
