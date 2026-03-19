cffc06daf856d33510dc33af6824000e8eb810c6
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-19 22:16:41 +0700

    bài 16

---
+++ bài 16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace bài_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            List<int> numbers = new List<int> { 7, 2, 12, 4, 33, 9, 7, 6 };
+            var number = numbers.Sum();
+            Console.WriteLine("Tổng các phần tử: "+ number);
+            Console.ReadKey();
+        }
+    }
+}
