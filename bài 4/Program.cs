0495e738fe0770592f9f1b287d354a878329a3d7
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-19 21:36:56 +0700

    bài 4

---
+++ bài 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace bài_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            List<int> numbers = new List<int> { 7, 2, 12, 4, 33, 9, 7, 0 };
+            var evenNumbers = from n in numbers
+                              orderby n descending
+                              select n;
+            Console.WriteLine("Sắp xếp từ lớn đến bé: " + string.Join(", ", evenNumbers));
+            Console.ReadKey();
+        }
+    }
+}
