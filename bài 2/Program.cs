c2056cdad7b1939e5ec66fe640d04cdccf0d654e
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-19 21:27:49 +0700

    bài 2

---
+++ bài 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace bài_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var evenNumbers = numbers.Where(n => n > 5);
+            Console.WriteLine("Các số lớn hơn 5: " + string.Join(", ", evenNumbers));
+            Console.ReadKey();
+        }
+    }
+}
